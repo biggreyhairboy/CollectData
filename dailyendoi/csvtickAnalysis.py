cce23cc7ec633e10942a970ee22a227a75ebcba9
patrick <biggreyhairboy@gmail.com>
2019-03-11 06:19:33 +0800

    changing from https to ssh to avoid login every time

--- dailyendoi/csvtickAnalysis.py
+++ dailyendoi/csvtickAnalysis.py
@@ -5,4 +5,9 @@ config = open('config.json')
 setting = json.load(config)
 df = DataFrame.from_csv('daystick.csv')
 # print(df['lastPrice'][:5])
-print(df.shape())
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    # print(df[:][0:5])
+
+
+morning_five_min = df['time' < time(9,5,0), 'time' >= time(9,0,0)]
+print(morning_five_min[:][0:5])
