7b5493e1a3d007b7ca52bd99d5c208db0ead4d11
patrick <biggreyhairboy@gmail.com>
2019-03-16 18:40:27 +0800

    read from pandas and convert time and datetime

--- dailyendoi/csvtickAnalysis.py
+++ dailyendoi/csvtickAnalysis.py
@@ -1,13 +1,20 @@
 import pandas as pd
 from pandas import DataFrame, Series
+from datetime import datetime, time
 import json
-config = open('config.json')
+config = open('dailyendoi/config.json')
 setting = json.load(config)
-df = DataFrame.from_csv('daystick.csv')
+df = pd.read_csv('dailyendoi/daystick.csv', parse_dates=True)
 # print(df['lastPrice'][:5])
 # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     # print(df[:][0:5])
-
-
-morning_five_min = df['time' < time(9,5,0), 'time' >= time(9,0,0)]
-print(morning_five_min[:][0:5])
+df.dropna()
+df['datetime'] = pd.to_datetime(df['datetime'], format='%Y-%m-%d %H:%M:%S.%f')
+df['time'] = pd.to_datetime(df['time'], format='%H:%M:%S.%f').dt.time
+# morning_five_min = df.loc[(df['time'] < time(9, 0, 5)) & (df['time'] >= time(9, 0, 0)), 'time']
+morning_five_min = df[(df['time'] <= time(9, 0, 5)) &
+                      (df['time'] >= time(9, 0, 0))]
+# print(df['time'] < time(9, 5, 0))
+# print(df.dtypes)
+# print(morning_five_min)
+print(morning_five_min.shape)
